9fca918575f99a3ad021bd92b1e82d52901118df
Olawale Lawal <lawalolawale@gmail.com>
2026-02-23 19:33:15 +0100

    Use nullable type hints in constructors and method signatures

--- Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppRequests/atEppUndeleteRequest.php
+++ Protocols/EPP/eppExtensions/at-ext-epp-1.0/eppRequests/atEppUndeleteRequest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Metaregistrar\EPP;
 
 
@@ -6,12 +7,14 @@ class atEppUndeleteRequest extends eppDomainRequest
 {
     use atEppCommandTrait;
 
+    protected atEppExtensionChain $atEppExtensionChain;
+
     /**
      * atEppUndeleteRequest constructor.
      * @param eppDomain $domain
      * @param atEppExtensionChain|null $atEppExtensionChain
      */
-    function __construct(eppDomain $domain, atEppExtensionChain $atEppExtensionChain = null) {
+    function __construct(eppDomain $domain, ?atEppExtensionChain $atEppExtensionChain = null) {
         $this->atEppExtensionChain = $atEppExtensionChain;
 
         parent::__construct(eppRequest::TYPE_UPDATE);
@@ -30,7 +33,7 @@ class atEppUndeleteRequest extends eppDomainRequest
     protected function setUndeleteExt() {
 
         $updExt = $this->createElement('rgp:update');
-        $updExt->setAttribute('xmlns:rgp',"urn:ietf:params:xml:ns:rgp-1.0" );
+        $updExt->setAttribute('xmlns:rgp', "urn:ietf:params:xml:ns:rgp-1.0");
         $updExt->setAttribute('xsi:schemaLocation', "urn:ietf:params:xml:ns:rgp-1.0 rgp-1.0.xsd");
 
         $restoreCmd = $this->createElement('rgp:restore');
@@ -39,6 +42,5 @@ class atEppUndeleteRequest extends eppDomainRequest
         $this->getExtension()->appendChild($updExt);
         $this->setAtExtensions();
     }
-
 
 }
